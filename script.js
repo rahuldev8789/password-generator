ac3992573beef07c5a0672f33fe9df71db6b3878
rahuldev8789 <rahul.mndl7686@gmail.com>
2026-07-16 04:54:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const characters = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M",
+  "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+  "a","b","c","d","e","f","g","h","i","j","k","l","m",
+  "n","o","p","q","r","s","t","u","v","w","x","y","z",
+  "0","1","2","3","4","5","6","7","8","9",
+  "!","@","#","$","%","^","&","*","(",")",
+  "_","+","-","=","[","]","{","}","|",";",
+  ":",",",".","<",">","?","/","~"
+];
+
+
+
+
+let firstEl = document.getElementById("first-pw");
+let secondEl = document.getElementById("second-pw");
+
+function generate() {
+  let pass1 = "";
+  let pass2 = "";
+  for (let i = 0; i < 15; i++) {
+    pass1 += characters[Math.floor(Math.random() * characters.length)];
+    pass2 += characters[Math.floor(Math.random() * characters.length)];
+  }
+  firstEl.textContent = pass1;
+  secondEl.textContent = pass2;
+}
